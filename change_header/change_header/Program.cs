d3ddd42b2d21f8a45fd2bf6181e050854cdc09c4
IsraeliKyozouTeam <guyb2000@gmail.com>
2017-07-05 15:51:11 +0300

    Add files via upload

---
+++ change_header/change_header/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Xml;
+using System.Configuration;
+using System.Xml.Linq;
+
+
+
+namespace change_header
+{
+    class Program
+    {
+        public static Dictionary<string, string> dict = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        public static void InitDictionary()
+        {
+             XmlReader reader = XmlReader.Create(ConfigurationSettings.AppSettings["headersmap"]);
+            while(reader.Read())
+            {
+                if (reader.Name=="header")
+                {
+                    while (reader.MoveToNextAttribute())
+                    {// Read the attributes.
+                        string x = reader.Value;
+                        dict.Add(x,"");
+                        reader.MoveToNextAttribute();
+                        dict[x] = reader.Value;
+                        
+                    }
+                }
+            }
+        }
+        
+            static void Main(string[] args)
+        {
+            InitDictionary();
+            string url = ConfigurationSettings.AppSettings["filepath"];
+            if (url.Contains("xlsx") || url.Contains("xls"))
+            {
+                Console.WriteLine("please enter-two tabs for two tabs");
+                Console.WriteLine("if you want just one tab, please enter something else");
+                if (Console.ReadLine() == "two tabs")
+                {
+                    exel x = new exel(url);
+                    x.openAnotherFile();
+                    x.changeDataOfFile(dict);
+                    x.closeFile();
+                    Console.WriteLine(x.xlApp);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    exel x = new exel(url);
+                    x.changeDataOfFile(dict);
+                    x.closeFile();
+                    Console.WriteLine(x.xlApp);
+                    Console.ReadKey();
+                }
+            }
+            if (url.Contains("csv")||url.Contains("txt"))
+            {
+                exel x = new exel(url);
+                x.changeDataOfFile(dict);
+                x.closeFile();
+                Console.WriteLine(x.xlApp);
+                Console.ReadKey();
+            }
+        }
+
+
+
+    }
+}
---
+++ change_header/change_header/exel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Configuration;
+
+namespace change_header
+{
+    interface IReadChangeFile
+    {
+        void changeDataOfFile(Dictionary<string,string> dict);
+        void openAnotherFile();
+        void closeFile();
+    }
+
+    public class exel : IReadChangeFile
+    {
+        public Excel.Application xlApp;
+        public Excel.Workbook xlWorkbook;
+        public Excel._Worksheet xlWorksheet;
+        public Excel.Range xlRange;
+        public exel(string name)
+        {
+            this.xlApp = new Excel.Application();
+            this.xlWorkbook = this.xlApp.Workbooks.Open(name);
+            this.xlWorksheet = this.xlWorkbook.Sheets[1];
+            xlWorksheet.Name = ConfigurationSettings.AppSettings["newworksheet"];
+            this.xlRange = this.xlWorksheet.UsedRange;
+        }
+        public void changeDataOfFile(Dictionary<string,string> dict)
+        {
+            for (int i = 1; i <= xlRange.Columns.Count; i++)
+            {
+                xlRange[1, i].value2 = dict[xlRange[1, i].value2];
+            }
+        }
+        public void openAnotherFile()
+        {
+            xlWorksheet = xlWorkbook.Sheets.Add();
+            xlWorksheet.Name = ConfigurationSettings.AppSettings["oldworksheet"]; 
+            for (int i = 1; i <= xlRange.Rows.Count; i++)
+            {
+                for (int j = 1; j <= xlRange.Columns.Count; j++)
+                {
+                    xlWorksheet.Cells[j, i].value2 = xlRange[j, i].value2;
+
+                    // Console.WriteLine("i={0},j={1}", i, j);
+                }
+            }
+        }
+        public void closeFile()
+        {
+            xlApp.ActiveWorkbook.Save();
+            xlWorkbook.Close(false, Type.Missing, Type.Missing);
+            xlApp.Quit();
+            GC.Collect();
+        }
+        
+    }
+}
